f3fbabe3d1ee154a6681b6779dba96d0092f959f
harB33 <bautistaharvy@gmail.com>
2026-05-01 21:02:15 +0800

    revert

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((err, req, res, next) => {
 
 if (require.main === module) {
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`)
+        console.log(`Server is running on http://localhost:${PORT}`)
     })
 }
 
